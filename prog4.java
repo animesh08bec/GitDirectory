aedb3e45abcf4ead197195289846bcf18178af51
Animesh Kumar Pal <animeshkrpal.iit@gmail.com>
2022-05-20 21:55:53 +0530

    new change

--- prog4.java
+++ prog4.java
@@ -1,6 +1,6 @@
 public class prog4 {
     public static void main(String[] args){
     System.out.println("This is program 4");
-    System.out.println("Included this file in gitignore");
+    System.out.println("Included this file in gitignore !");
     }
 }
